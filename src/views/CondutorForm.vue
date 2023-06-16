e6862bc2260adc6506d9d4295d0a1da45441d3dd
Matheus Almeida <matheusgalmeida6@gmail.com>
2023-06-16 00:36:07 -0300

    try: CondutorForm

--- src/views/CondutorForm.vue
+++ src/views/CondutorForm.vue
@@ -1,21 +1,69 @@
 <script lang="ts">
+import { Condutor } from '@/model/condutor';
+import { CondutorCliente } from '@/client/condutor.client';
+import { Notification } from '@/model/notification'
 
+export default class condutorForm extends Condutor {
+
+  public notification : Notification = new Notification()
+  public condutor : Condutor = new Condutor
+  public CondutorCliente! : CondutorCliente
+
+  public mounted(): void {
+    this.CondutorCliente = new CondutorCliente()
+      console.log(this.nome)
+      console.log(this.telefone)
+      console.log(this.cpf)
+  }
+
+  public onClickCadastrar(): void {
+    this.CondutorCliente.cadastrar(this.condutor)
+      .then(
+        success => {
+          this.notification = this.notification.new(true, 'notification is-success', 'Condutor cadastrado com sucesso!!!');
+          this.onClickLimpar();
+      },
+      error => {
+        this.notification = this.notification.new(true, 'notification is-danger', 'Error: ' + error);
+        this.onClickLimpar();
+      });
+  }
+
+  public onClickFecharNotificacao(): void {
+    this.notification = new Notification()
+  }
+  public onClickLimpar(): void {
+    this.condutor = new Condutor()
+  }
+  public setNotification(active: boolean, className: string, message: string): void {
+  this.notification = this.notification.new(active, className, message);
+}
+}
 </script>
 
 <template>
-  <form>
-    <div class="row">
-      <div class="col">
-        <input type="text" class="form-control" placeholder="Nome" v-model="nome" aria-label="First name">
-      </div>
-      <div class="col">
-        <input type="text" class="form-control" placeholder="Telefone" v-model="telefone" aria-label="First name">
-      </div>
-      <div class="col">
-        <input type="text" class="form-control" placeholder="CPF" v-model="cpf" aria-label="Last name">
+  <div class="columns" v-if="notification.ativo">
+    <div class="column is-12">
+      <div :class="notification.classe">
+      <button @click="onClickFecharNotificacao()" class="delete" ></button>
+        {{ notification.mensagem }}
       </div>
     </div>
-    <button type="submit" class="btn">Cadastrar</button>
+  </div>
+
+  <form >
+    <div class="row">
+      <div class="col">
+        <input type="text" class="form-control" placeholder="Nome" v-model="condutor.nome" aria-label="Nome">
+      </div>
+      <div class="col">
+        <input type="text" class="form-control" placeholder="Telefone" v-model="condutor.telefone" aria-label="Telefone">
+      </div>
+      <div class="col">
+        <input type="text" class="form-control" placeholder="CPF" v-model="condutor.cpf" aria-label="CPF">
+      </div>
+    </div>
+    <button type="submit" class="btn" @click="onClickCadastrar()">Cadastrar</button>
   </form>
 </template>
 
